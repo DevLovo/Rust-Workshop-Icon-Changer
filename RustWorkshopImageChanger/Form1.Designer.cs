c994e50e749eac3feff1584e5e325cd8b9e3b894
DevLovo <therandomgaming77@gmail.com>
2021-08-10 17:02:34 +1000

    v1

---
+++ RustWorkshopImageChanger/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace RustWorkshopImageChanger
+{
+    partial class RWIC
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RWIC));
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.setImage = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // preview
+            // 
+            this.preview.BackColor = System.Drawing.SystemColors.Window;
+            this.preview.Image = ((System.Drawing.Image)(resources.GetObject("preview.Image")));
+            this.preview.Location = new System.Drawing.Point(12, 12);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(256, 256);
+            this.preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.preview.TabIndex = 0;
+            this.preview.TabStop = false;
+            this.preview.Click += new System.EventHandler(this.preview_Click);
+            this.preview.DragDrop += new System.Windows.Forms.DragEventHandler(this.preview_DragDrop);
+            this.preview.DragEnter += new System.Windows.Forms.DragEventHandler(this.preview_DragEnter);
+            // 
+            // setImage
+            // 
+            this.setImage.Location = new System.Drawing.Point(12, 274);
+            this.setImage.Name = "setImage";
+            this.setImage.Size = new System.Drawing.Size(256, 30);
+            this.setImage.TabIndex = 1;
+            this.setImage.Text = "Set Image";
+            this.setImage.UseVisualStyleBackColor = true;
+            this.setImage.Click += new System.EventHandler(this.setImage_Click);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 310);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.Size = new System.Drawing.Size(256, 128);
+            this.log.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // RWIC
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(280, 450);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.setImage);
+            this.Controls.Add(this.preview);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "RWIC";
+            this.Text = "RWIC";
+            this.Load += new System.EventHandler(this.RWIC_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.RWIC_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.RWIC_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.Button setImage;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RustWorkshopImageChanger/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustWorkshopImageChanger
+{
+    public partial class RWIC : Form
+    {
+        string fileName = "";
+        List<string> updatedFolders = new List<string>();
+        string[] supportedTypes = { ".png", ".jpg", ".jpeg"};
+
+        public RWIC()
+        {
+            InitializeComponent();
+        }
+
+        private void preview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RWIC_Load(object sender, EventArgs e)
+        {
+            preview.AllowDrop = true;
+            printLog("Waiting for an image to be set!");
+
+            string filter = "Image";
+            foreach (string type in supportedTypes)
+            {
+                filter += "|*" + type;
+            }
+            openFileDialog1.Filter = filter;
+        }
+
+        private void preview_DragDrop(object sender, DragEventArgs e)
+        {
+            updatePreview(e);
+        }
+
+        private void updatePreview(DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+
+                    if (supportedTypes.Contains(Path.GetExtension(fileNames[0])))
+                    {
+                        preview.ImageLocation = fileNames[0];
+                        fileName = fileNames[0];
+                        printLog("Image selected successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("File type not supported!" + Environment.NewLine + "Supported types: " + getTypes());
+
+                    }
+                }
+            }
+        }
+
+        private void preview_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+
+        }
+
+        private void RWIC_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void RWIC_DragDrop(object sender, DragEventArgs e)
+        {
+            updatePreview(e);
+        }
+
+        private void printLog(string text)
+        {
+            log.AppendText(text);
+            log.AppendText(Environment.NewLine);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string tempDIR = Path.GetTempPath();
+            if (fileName != "")
+            {
+                if (File.Exists(fileName))
+                {
+                    string[] subdirectoryEntries = Directory.GetDirectories(tempDIR);
+                    foreach(string dir in subdirectoryEntries)
+                    {
+                        string iconDIR = dir + @"\icon_background.png";
+                        if (!updatedFolders.Contains(dir))
+                        {
+                            if (dir.EndsWith(".tmp") & Path.GetFileName(dir).StartsWith("tmp"))
+                            {
+                                if (File.Exists(iconDIR))
+                                {
+                                    try
+                                    {
+                                        File.Delete(iconDIR);
+                                        File.Copy(fileName, iconDIR);
+                                        printLog("Image changed successfully!");
+                                        updatedFolders.Add(dir.ToString());
+                                    }
+                                    catch
+                                    {
+                                        printLog("Image failed to change!");
+
+                                    }
+
+                                    break;
+
+                                }
+
+                            }
+                        }
+                            
+                    }
+                }
+            }
+        }
+
+        private string getTypes()
+        {
+            string String = "";
+            foreach(string type in supportedTypes)
+            {
+                String += type + " ";
+            }
+            return (String.Remove(String.Length - 1));
+        }
+        private void setImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            if(openFileDialog1.FileName != "" & supportedTypes.Contains(Path.GetExtension(openFileDialog1.FileName)))
+            {
+                fileName = openFileDialog1.FileName;
+                preview.ImageLocation = fileName;
+                printLog("Image selected successfully!");
+            }
+            else
+            {
+                if(openFileDialog1.FileName != "")
+                {
+                    MessageBox.Show("File type not supported!" + Environment.NewLine + "Supported types: " + getTypes());
+
+                }
+            }
+        }
+    }
+}
